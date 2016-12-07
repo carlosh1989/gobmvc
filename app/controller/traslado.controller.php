969de5f78b0bdc9b6aceb7e9bcc2144dfa7d2533
Od@ <kennymartorres@kennymartorres>
2016-12-07 09:41:07 -0400

    agregado en el orm la funcion guardar

--- app/controller/traslado.controller.php
+++ app/controller/traslado.controller.php
@@ -17,7 +17,6 @@ class TrasladoController extends MvcController
 
     public function create()
     {
-        $this->decreto = $this->orm->buscar('decretos',1);
         $this->vista('create', 'Crear Nuevo Decreto');
     }
 
@@ -27,48 +26,7 @@ class TrasladoController extends MvcController
         $data['numero'] = $numero;
         $data['descripcion'] = $descripcion;
         $this->orm->guardar('decretos',$data);
-        //$this->ir_con_datos('traslado',$data);
-        //$this->ver_arreglo($data);
-      /*  $table = "decretos";
-        $array = $data;
-       $str = "insert into $table ";
-       $strv = "";
-       while(list($name,$value) = each($array)) {
-
-           if(is_bool($value)) {
-                    $strn .= "$name,";
-                    $strv .= ($value ? "true":"false") . ",";
-                    continue;
-            };
-
-           if(is_string($value)) {
-                    $strn .= "$name,";
-                    $strv .= "'$value',";
-                    continue;
-            }
-           if (!is_null($value) and ($value != "")) {
-                    $strn .= "$name,";
-                    $strv .= "$value,";
-                    continue;
-           }
-       }
-
-       $strv[strlen($strv)-1] = '';
-       $str .= $strn . $strv;
-       echo $strn; //numero , descripcion
-       echo "<hr>";
-       echo $strv; //'259/16','333'*/
-    }
-
-    
-    public function guardar()
-    {
-        var_dump($_REQUEST);
-        $_REQUEST['id'] > 0
-            ? $this->traslado->actualizar($_REQUEST)
-            : $this->traslado->registrar($_REQUEST);
-        
-        header('Location: rol.php');
+        $this->ir('traslado');
     }
     
     public function eliminar()
--- app/model/orm.class.php
+++ app/model/orm.class.php
@@ -2,6 +2,17 @@
 require_once("db.class.php");
 class Orm
 {
+ //nombre base de datos
+    private $dbname = "dbp";
+ //nombre servidor
+    private $host = "localhost";
+ //nombre usuarios base de datos
+    private $user = "postgres";
+ //password usuario
+    private $pass = '123';
+ //puerto postgreSql
+    private $port = 5432;
+    private $dbh;
     public $pdo;
 
     public function __construct()
@@ -74,44 +85,44 @@ class Orm
 
     public function guardar($table,$array)
     {
-       $str = "insert into $table ";
-       $strn = "";
-       $strv = "";
-        while(list($name,$value) = each($array)) {
+ $str = "insert into $table ";
+   $strn = "(";
+   $strv = " VALUES (";
+   while(list($name,$value) = each($array)) {
 
-           if(is_bool($value)) {
-                    $strn .= "$name";
-                    $strv .= ($value ? "true":"false") . "";
-                    continue;
-            };
+       if(is_bool($value)) {
+                $strn .= "$name,";
+                $strv .= ($value ? "true":"false") . ",";
+                continue;
+        };
 
-           if(is_string($value)) {
-                    $strn .= "$name,";
-                    $strv .= "'$value',";
-                    continue;
-            }
-           if (!is_null($value) and ($value != "")) {
-                    $strn .= "$name,";
-                    $strv .= "$value,";
-                    continue;
-           }
+       if(is_string($value)) {
+                $strn .= "$name,";
+                $strv .= "'$value',";
+                continue;
+        }
+       if (!is_null($value) and ($value != "")) {
+                $strn .= "$name,";
+                $strv .= "$value,";
+                continue;
        }
+   }
+   $strn[strlen($strn)-1] = ')';
+   $strv[strlen($strv)-1] = ')';
+   $str .= $strn . $strv;
+   //return $str;
 
-       $strn[strlen($strv)-0] = '';
-       $strv[strlen($strv)-1] = '';
-       $str .= $strn . $strv;
        //echo $strn; //numero , descripcion
        //echo "<hr>";
        //echo $strv; //'259/16','333'
 
-        try {
-            $sql = "INSERT INTO " . $table . " (" . $strn . ") 
-                    VALUES (" . $strv . ")";
-            echo $sql;
-            //$this->pdo->execute($sql);
-        //$this->registrarRolAccion($data);
-        } catch (Exception $e) {
-            die($e->getMessage());
-        }
+
+    //$sql = "INSERT INTO " . $table . " (" . $strn . ") 
+                    //VALUES (" . $strv . ")";
+
+   $dbconn = pg_connect("host=localhost port=5432 dbname=dbp user=postgres password=123")
+    or die('NO HAY CONEXION: ' . pg_last_error());
+
+    $result = pg_query($dbconn, $str) or die('ERROR AL INSERTAR DATOS: ' . pg_last_error());
     }
 }
