d17192ec8ab6120114ad2b5f029f4e1eac452f5c
Od@ <elmorochez22@gmail.com>
2016-12-12 12:01:39 -0400

    AAgregando archivo -consola/revizar.sh-

---
+++ consola/revizar.sh
@@ -0,0 +1,8 @@
+	#!/bin/bash
+
+# -*- ENCODING: UTF-8 -*-
+
+#proyecto=@@nombre_proyecto@@
+
+./vendor/bin/phpcs --colors ./app/controller/$1.controller.php
+./vendor/bin/phpcs ./app/controller/$1.controller.php --report=diff
