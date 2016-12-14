6a97f1ad159a1de6778b218242ef613fb82075f7
Od@ <elmorochez22@gmail.com>
2016-12-14 16:02:58 -0400

    Agregando comentario

--- app/controller/traslado.controller.php
+++ app/controller/traslado.controller.php
@@ -53,6 +53,7 @@ class TrasladoController extends MvcController
 
         $this->detallesSuma = $monto_actual;
         //$this->ver_arreglo($this->detallesSuma);
+        //asdsa
         $this->vista('show', 'Decreto');
     }
     
