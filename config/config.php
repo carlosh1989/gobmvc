1e76d2e953a728e8250d450b0c31be7250ef2182
Od@ <elmorochez22@gmail.com>
2016-12-27 11:39:25 -0400

    asd

---
+++ config/config.php
@@ -0,0 +1,3 @@
+<?php
+// Url Base usado para los estaticos y los links
+define('baseUrl', 'http://localhost/gobmvc/');
---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+//Configuración default de la base de datos
+define('DB_ADAPTER', 'pgsql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'frame');
+define('DB_USER', 'postgres');
+define('DB_PASSWORD', '123');
+define('DB_PORT', 5432);
+
