c9183ffbfc3e7ab247c3f4fa996a5c91a9cf27b8
Od@ <elmorochez22@gmail.com>
2016-12-27 10:32:11 -0400

    asd

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'frame');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'Adm15.');
+define('DB_PORT', 3306);
---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'dbp');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '123');
+define('DB_PORT', 5432);
