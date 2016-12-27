bfc02ced9da37e575b2db661895572530e661f3a
Od@ <elmorochez22@gmail.com>
2016-12-27 15:51:56 -0400

    testeando las migraciones

--- config/db.php
+++ config/db.php
@@ -1,8 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'dbp');
+define('DB_NAME', 'frame');
 define('DB_USER', 'postgres');
 define('DB_PASSWORD', '123');
 define('DB_PORT', 5432);
 
- 
---
+++ migrations/20161227194131_mi_primera_migracion.php
@@ -0,0 +1,54 @@
+<?php
+
+use \MyProject\Migration\Migration;
+
+class MiPrimeraMigracion extends Migration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        // create the table
+        $table = $this->table('usuarios');
+        $table->addColumn('user_id', 'integer')
+        ->addColumn('nombre','string')
+        ->addColumn('email','string')
+        ->addColumn('created', 'datetime')
+        ->create();
+    }
+
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+
+    }
+}
