ca46bd07fcc0e111f093c33c0d1ba140482e2374
Od@ <elmorochez22@gmail.com>
2017-01-10 09:23:35 -0400

    agregado configuracion de db

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_ADAPTER', 'pgsql');
+define('DB_ADAPTER', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'frame');
-define('DB_USER', 'postgres');
-define('DB_PASSWORD', '123');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'Adm15.');
 define('DB_PORT', 5432);
 
